683f83770b8ab1c7d4e28a8b30f7742bef25ac76
Sam Johnson <sam@durosoft.com>
2018-07-17 01:28:47 -0400

    progress

--- src/gcf_test.cr
+++ src/gcf_test.cr
@@ -2,11 +2,7 @@ require "gcf"
 
 class GcfTest < GCF::CloudFunction
   def run
-    console.log "welcome to the test"
-    sleep 3
-    console.warn "this is a warning"
-    console.error "this is an error"
-    console.log "hey\nthis\nhas\nnewlines"
-    send_file "./src/gcf_test.cr"
+    console.log "before error"
+    raise "this is america"
   end
 end
